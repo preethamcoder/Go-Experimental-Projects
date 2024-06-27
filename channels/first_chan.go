26ad653748cfd5f9270bfb69bb584963358a43d1
Preetham Thelluri <preetham@Preethams-MacBook-Air.local>
2024-06-27 18:44:26 -0400

    Tried revesing an array

--- channels/first_chan.go
+++ channels/first_chan.go
@@ -20,3 +20,21 @@ func main() {
 	}
 
 }
+
+// package main
+
+// import "fmt"
+
+// func reverse(s []int) []int {
+// 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+// 		s[i], s[j] = s[j], s[i]
+// 	}
+// 	return s
+// }
+
+// func main() {
+// 	slice := []int{1, 2, 3, 4, 5}
+// 	fmt.Println("Original slice:", slice)
+// 	reversedSlice := reverse(slice)
+// 	fmt.Println("Reversed slice:", reversedSlice)
+// }
